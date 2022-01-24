4032dca8aaa7d898b12241cfd446bbe0bfa85608
marcoalonso <marcoalonsorota@hotmail.com>
2022-01-24 13:19:26 -0600

    Empezando

---
+++ SimpleTableViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleTableViews
+//
+//  Created by marco rodriguez on 24/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
